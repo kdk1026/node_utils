4db1afa4ca778cbdcaed83573d2f76d8c3cb1432
kdk1026 <daekwang1026@gmail.com>
2026-02-14 18:12:14 +0900

    업데이트

--- test/file/file-test.js
+++ test/file/file-test.js
@@ -10,12 +10,12 @@ console.log( 'readableFileSize : ', readableFileSize(8039723008) );
 
 console.log( 'lastModified : ', lastModified('D:/test/test.txt') );
 
-// writeFile('D:/test/hello_write.txt', '안녕~~~');
+writeFile('D:/test/hello_write.txt', '안녕~~~');
 
 console.log( 'readFile : ', readFile('D:/test/test.txt') );
-// console.log( 'deleteFile : ', deleteFile('D:/test/test2.txt') );
+console.log( 'deleteFile : ', deleteFile('D:/test/test2.txt') );
 
-// copyFile('D:/test/test.txt', 'D:/test/test_copy.txt');
+copyFile('D:/test/test.txt', 'D:/test/test_copy.txt');
 
 console.log( 'getAllFileList : ', getAllFileList('D:/test') );
 console.log( 'getFileList : ', getFileList('D:/test') );
